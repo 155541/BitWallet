f8eeb3f261725bfe6a77b94c54d32799e47157c2
splanes <155541.clot@fje.edu>
2018-11-20 14:56:36 +0100

    fingerprint helper

---
+++ app/src/main/java/revolhope/splanes/com/bitwallet/helper/FingerprintHelper.java
@@ -0,0 +1,166 @@
+package revolhope.splanes.com.bitwallet.helper;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.os.CancellationSignal;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public abstract class FingerprintHelper {
+
+    private static final String NAME = "finger-key";
+
+    public static class Util
+    {
+        private KeyStore keyStore;
+        private Cipher cipher;
+
+        public void genKey()  {
+            try {
+
+                keyStore = KeyStore.getInstance("AndroidKeyStore");
+                KeyGenerator keyGenerator = KeyGenerator.getInstance(
+                        KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+                keyStore.load(null);
+                KeyGenParameterSpec.Builder builder = new KeyGenParameterSpec.Builder(
+                                NAME,
+                                KeyProperties.PURPOSE_ENCRYPT |
+                                        KeyProperties.PURPOSE_DECRYPT);
+
+                builder.setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                        .setUserAuthenticationRequired(true)
+                        .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7);
+
+                keyGenerator.init(builder.build());
+
+                keyGenerator.generateKey();
+
+
+            }
+            catch ( KeyStoreException |
+                    NoSuchAlgorithmException |
+                    NoSuchProviderException |
+                    IOException |
+                    CertificateException |
+                    InvalidAlgorithmParameterException e) {
+
+            }
+        }
+
+        public boolean initCipher() {
+
+            try {
+                cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" +
+                                            KeyProperties.BLOCK_MODE_CBC + "/" +
+                                            KeyProperties.ENCRYPTION_PADDING_PKCS7);
+            }
+            catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            try {
+                keyStore.load(null);
+                SecretKey key = (SecretKey) keyStore.getKey(NAME, null);
+                cipher.init(Cipher.ENCRYPT_MODE, key);
+                return true;
+            }
+            catch (KeyStoreException |
+                    CertificateException |
+                    UnrecoverableKeyException |
+                    IOException |
+                    NoSuchAlgorithmException |
+                    InvalidKeyException e) {
+
+                e.printStackTrace();
+                return false;
+            }
+
+        }
+
+        public Cipher getCipher() { return cipher; }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public static class AuthCallback extends FingerprintManager.AuthenticationCallback
+    {
+        private CancellationSignal cancellationSignal;
+        private final Context context;
+
+        public AuthCallback(Context mContext) {
+            context = mContext;
+        }
+
+        // Implement the startAuth method,
+        // which is responsible for starting the fingerprint authentication process
+
+        public void startAuth(FingerprintManager manager,
+                              FingerprintManager.CryptoObject cryptoObject) {
+
+            cancellationSignal = new CancellationSignal();
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            manager.authenticate(cryptoObject,
+                                 cancellationSignal,
+                                0,
+                                this,
+                                null);
+        }
+
+        // onAuthenticationError is called when a fatal error has occurred.
+        // It provides the error code and error message as its parameters
+        @Override
+        public void onAuthenticationError(int errMsgId, CharSequence errString) {
+            Toast.makeText(context, "Authentication error:\n" + errString, Toast.LENGTH_LONG)
+                 .show();
+        }
+
+        // onAuthenticationFailed is called when the fingerprint does not match
+        // with any of the fingerprints registered on the device
+        @Override
+        public void onAuthenticationFailed() {
+            Toast.makeText(context, "Authentication failed", Toast.LENGTH_LONG).show();
+        }
+
+        // onAuthenticationHelp is called when a non-fatal error has occurred.
+        // This method provides additional information about the error,
+        // so to provide the user with as much feedback as possible
+        // I’m incorporating this information into my toast
+        @Override
+        public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+            Toast.makeText(context, "Authentication help\n" + helpString, Toast.LENGTH_LONG)
+                 .show();
+        }
+
+        // onAuthenticationSucceeded is called when a fingerprint has been successfully matched
+        // to one of the fingerprints stored on the user’s device
+        @Override
+        public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+            Toast.makeText(context, "Success!", Toast.LENGTH_LONG).show();
+        }
+    }
+}
--- app/src/main/java/revolhope/splanes/com/bitwallet/view/CreateNewActivity.java
+++ app/src/main/java/revolhope/splanes/com/bitwallet/view/CreateNewActivity.java
@@ -3,6 +3,10 @@ package revolhope.splanes.com.bitwallet.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import revolhope.splanes.com.bitwallet.R;
 
@@ -15,7 +19,21 @@ public class CreateNewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create);
 
 
+        TextView textView_Id = findViewById(R.id.textView_Id);
 
+        EditText editText_Account = findViewById(R.id.editText_Account);
+        EditText editText_URL = findViewById(R.id.editText_URL);
+        EditText editText_User = findViewById(R.id.editText_User);
+        EditText editText_Password = findViewById(R.id.editText_Password);
 
+        Button btGenerate = findViewById(R.id.btGenerate);
+
+        CheckBox checkBox_Expire = findViewById(R.id.checkBox_Expire);
+        TextView textView_ExpireDate = findViewById(R.id.textView_ExpireDate);
+
+        EditText editText_Brief = findViewById(R.id.editText_Brief);
+
+        Button btCreate = findViewById(R.id.btCreate);
+        Button btCancel = findViewById(R.id.btCancel);
     }
 }
