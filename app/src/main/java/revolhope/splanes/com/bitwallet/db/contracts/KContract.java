04f72fb4a3b8bfc58b4905fb39ef6a2bf9f79db7
155541 <34254684+155541@users.noreply.github.com>
2018-11-21 12:57:40 +0100

    Update KContract.java

--- app/src/main/java/revolhope/splanes/com/bitwallet/db/contracts/KContract.java
+++ app/src/main/java/revolhope/splanes/com/bitwallet/db/contracts/KContract.java
@@ -5,14 +5,22 @@ public interface KContract {
     String TABLE = "K";
 
     String COLUMN_ID = "_ID";
-    String COLUMN_K = "_K";
-    String COLUMN_CHECKSUM = "CHECKSUM";
+    String COLUMN_ACC_ID = "ACC_ID";
+    String COLUMN_CRYPTO_PWD = "CRYPTO";
+    String COLUMN_PARAMS = "PARAMS";
+    String COLUMN_DEADLINE = "DEADLINE";
 
-    String[] COLUMNS = new String[] {COLUMN_ID, COLUMN_K, COLUMN_CHECKSUM};
+    String[] COLUMNS = new String[] {COLUMN_ID, COLUMN_ACC_ID, COLUMN_CRYPTO_PWD,
+                                     COLUMN_PARAMS, COLUMN_DEADLINE};
 
     String STATEMENT_CREATE =
-            "CREATE TABLE K("+
+            "CREATE TABLE K(" +
             COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-            COLUMN_K + " BLOB NOT NULL," +
-            COLUMN_CHECKSUM + " BLOB NOT NULL)";
+            COLUMN_ACC_ID + " VARCHAR() NOT NULL,
+            COLUMN_CRYPTO_PWD + " BLOB NOT NULL," +
+            COLUMN_PARAMS + " BLOB NOT NULL," +
+            COLUMN_DEADLINE + " INTEGER NOT NULL " +
+            "CONTRAINT FK_K FOREIGN KEY (" + COLUMN_ACC_ID + ") REFERENCES " + 
+                AccountContract.TABLE + "(" + AccountContract.COLUMN_ID + "), " + 
+            "CONTRAINT U_K UNIQUE(" + COLUMN_ACC_ID + "))";
 }
