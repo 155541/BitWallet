c3edf78e6a78ee9cb2caba410275bf905c19897b
155541 <34254684+155541@users.noreply.github.com>
2018-11-21 13:58:31 +0100

    Update K.java

--- app/src/main/java/revolhope/splanes/com/bitwallet/model/K.java
+++ app/src/main/java/revolhope/splanes/com/bitwallet/model/K.java
@@ -4,21 +4,19 @@ public class K {
     private Long _id;
     private Long accId;
     private String pwdBase64;
-    private SecretKey _k;
     private GCMParameterSpec spec;
     private Long deadline;
     
-    public K (@NonNull _id, @NonNull accId, @NonNull pwdBase64, @NonNull SecretKey _k
+    public K (@NonNull _id, @NonNull accId, @NonNull pwdBase64
               @NonNull GCMParameterSpec spec, @NonNull deadline) {
         
         this._id = _id;
         this.accId = accId;
         this.pwdBase64 = pwdBase64;
-        this._k = _k;
         this.spec = spec;
         this.deadline = deadline;
     }
     
-    
+    public K() {}
     
 }
