06bca597e58cb7401c96033e61e78041564146dc
155541 <34254684+155541@users.noreply.github.com>
2018-11-22 13:56:33 +0100

    Update DialogHelper.java

--- app/src/main/java/revolhope/splanes/com/bitwallet/helper/DialogHelper.java
+++ app/src/main/java/revolhope/splanes/com/bitwallet/helper/DialogHelper.java
@@ -1,31 +1,73 @@
 
 
 public class DialogHelper {
-
-
-
- 
- 
-    public static class SampleDialog extends DialogFragment {
+   
+    private AlertDialog.Builder builder;
     
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.dialog_fire_missiles)
-               .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                       // FIRE ZE MISSILES!
-                   }
-               })
-               .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                       // User cancelled the dialog
-                   }
-               });
-        // Create the AlertDialog object and return it
-        return builder.create();
+    public DialogHelper(@NonNull Context context) {
+        builder = new AlertDialog.Builder(context);
+    }
+    
+    public void setStrings(@NonNull String title, @Nullable String message) {
+        builder.setTitle(title);
+        builder.setMessage(message);
+    }
+    
+    public void setPossitiveButton(@NonNull String positiveButton,
+                                   @NonNull DialogInterface.OnClickListener listener) {
+        builder.setPositiveButton(positiveButton, listener);
+    }
+    
+    public void setNeutralButton(@NonNull String neutralButton,
+                                 @NonNull DialogInterface.OnClickListener listener) {
+        builder.setNeutralButton(neutralButton, listener);
+    }
+    
+    public void setNegativeButton(@NonNull String negativeButton,
+                                  @NonNull DialogInterface.OnClickListener listener) {
+        builder.setNegativeButton(negativeButton, listener);
+    }
+    
+    public void setIcon(int drawableRes) {
+        builder.setIcon(drawableRes);
     }
         
+    public void setView(View view) {
+        builder.setView(view);
+    }
+    
+    public AlertDialog createDialog() {
+        return builder.create();
+    }
+    
+    public void show() {
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    
+    public static void showInfo(@NonNull String title, @NonNull String message, @NonNull Context context) {
+        DialogHelper help = new DialogHelper(context);
+        help.setStrings(title, message);
+        help.setPossitiveButton("Ok", new  DialogInterface.OnClickListener {
+            public void onClick(DialogInterface dialog, int id) {}
+        });
+        help.setIcon(R.drawable.ic_dialog_info);
+        help.show();
+    }
+    
+    public static AlertDialog showLoader(@NonNull String title,
+                                         @NonNull Context context, 
+                                         @NonNull DialogInterface.OnClickListener cancelListener) {
+        
+        Activity activity = (Activity) context;
+        ViewGroup vg = (ViewGroup) activity.findViewById(android.R.id.content);
+        
+        DialogHelper help = new DialogHelper(context);
+        help.setStrings(title, null);
+        help.setView(LayoutInflater.from(context).inflate(R.layout.dialog_loader, vg, false));
+        help.setNegativeButton("Cancel", cancelListener);
+        AlertDialog d = help.createDialog();
+        d.show();
+        return d;
     }
 }
