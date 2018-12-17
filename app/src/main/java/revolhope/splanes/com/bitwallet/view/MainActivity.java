b871accc5430d37d9afeeb84ab5f98c0ea6f8cfb
splanes <155541.clot@fje.edu>
2018-12-17 15:06:32 +0100

    Pujada abans de liarla xD

--- app/src/main/java/revolhope/splanes/com/bitwallet/view/MainActivity.java
+++ app/src/main/java/revolhope/splanes/com/bitwallet/view/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_backup) {
             return true;
         }
         if (id == R.id.home)
